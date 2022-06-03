0727db51b444f339cfdb89377b81b3707d164cf3
jisci <50617462+ZENOVERS@users.noreply.github.com>
2022-06-04 00:34:51 +0900

    Update s.c

--- s.c
+++ s.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
+#include <float.h>
+#include <math.h>
 #pragma warning(disable:4996)
 
-#define NOT_debug_mode_view_matrix
-//활성화하면 연산 수행마다 (입력받은 행렬)\n\n(역행렬) 형태로 시각적으로 출력
-
-
-//단위행렬인지 확인(단위행렬이면 1, 아니면 0 반환) 
-int is_I(float x[][100], int dim) {
-	int cnt = 0;
-
-	for (int i = 0; i < dim; i++)
-		for (int j = 0; j < dim; j++)
-			if ((i == j && x[i][j] != 1.0f) || (i != j && x[i][j] != 0.0f)) //단위행렬 조건 - 주대각성분이 모두 1, 나머지 성분은 모두 0
-				return 0;
-
-	return 1;
-}
+#define debug_mode_view_matrix
+//활성화하면('NOT_' 제거) 연산 수행마다 (현 연산 단계)\n\n(입력받은 행렬)\n\n(역행렬) 형태로 시각적으로 출력
 
 
 //대각행렬 및 역행렬이 존재하는지 확인
-int is_Diagonal(float x[][100], int dim)
+int is_Diagonal(double x[][100], int dim)
 {
 	for (int i = 0; i < dim; i++)
 		for (int j = 0; j < dim; j++)
@@ -33,36 +22,46 @@ int is_Diagonal(float x[][100], int dim)
 
 
 //역행렬 계산
-int get_inverse(float x[][100], float des[][100], int dim)
+int get_inverse(double x[][100], double des[][100], int dim)
 {
-	float mul = 0.0f;
+	double mul = 0.0f;
+	int modified_row = 0; //수정된 행의 색을 변화시키기 위해 수정된 행의 값 저장
 
 	//대각행렬로 만듦
 	for (int i = 0; i < dim; i++)
 	{
-		for (int j = 0; j < dim; j++)
+		for (int j = 0; j < dim; j++) //j - 행렬의 행 수
 		{
-			for (int k = 0; k < dim; k++)
-			{
-				if (i == j || x[i][i] == 0)
-					break;
+			if (i == j || x[i][i] == 0 || x[j][i] == 0) //분모, 분자가 0인 경우 continue
+				continue;
 
-				if (k == 0)
+			for (int k = 0; k < dim; k++) //k - 행렬의 열 수
+			{
+				modified_row = j;
+
+				if (k == 0) //처음 한 번만 실행
 					mul = -x[j][i] / x[i][i];
 
 				x[j][k] += mul * x[i][k];
 				des[j][k] += mul * des[i][k];
-			}
-			
-#ifdef debug_mode_view_matrix
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10); //글씨 색 초록색
 
-			puts("\n");
+				//실수 계산의 부동소수점 반올림 오차를 해결하기 위해 너무 작은 값은 0으로 처리
+				if (fabs(x[j][k]) <= FLT_EPSILON)
+					x[j][k] = 0.0f;
+
+				if (fabs(des[j][k]) <= FLT_EPSILON)
+					des[j][k] = 0.0f;
+			}
+
+#ifdef debug_mode_view_matrix
+			puts("\n\n\n\nSt.1 - Making Diagonal Matrix\n\n"); //대각행렬 만들기
 
 			for (int i = 0; i < dim; i++)
 			{
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (modified_row == i) ? 12 : 10); //일반적 - 연두색(10), 수정된 행 - 주황색(12)
+
 				for (int j = 0; j < dim; j++)
-					printf("%g ", x[i][j]);
+					printf("%11lg ", x[i][j]);
 
 				puts("");
 			}
@@ -71,24 +70,32 @@ int get_inverse(float x[][100], float des[][100], int dim)
 
 			for (int i = 0; i < dim; i++)
 			{
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (modified_row == i) ? 12 : 10);
+
 				for (int j = 0; j < dim; j++)
-					printf("%g ", des[i][j]);
+					printf("%11lg ", des[i][j]);
 
 				puts("");
 			}
-#endif
 
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+#endif
 		}
 	}
 
-	//위의 연산을 끝낸 상태에서 대각행렬이 아니면 역행렬은 존재하지 않음
+	//위의 연산을 끝낸 상태에서 대각행렬(주대각성분 != 0)이 아니면 역행렬은 존재하지 않음
 	if (is_Diagonal(x, dim) == 0)
 		return 0;
 
-	//대각행렬이 1이 되게 변환 -> 최종적으로 역행렬 계산
+	//주대각성분이 1이 되게 변환 -> 최종적으로 역행렬 계산
 	for (int i = 0; i < dim; i++)
 	{
 		mul = 1 / x[i][i];
+
+		if (mul == 1) //주대각성분이 1이면 나눌 필요 없음
+			continue;
+
+		modified_row = i;
 
 		for (int j = 0; j < dim; j++)
 		{
@@ -97,12 +104,14 @@ int get_inverse(float x[][100], float des[][100], int dim)
 		}
 
 #ifdef debug_mode_view_matrix
-		puts("\n");
+		puts("\n\n\n\nSt.2 - Making Identity Matrix\n\n"); //단위행렬 만들기
 
 		for (int i = 0; i < dim; i++)
 		{
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (modified_row == i) ? 12 : 10);
+
 			for (int j = 0; j < dim; j++)
-				printf("%g ", x[i][j]);
+				printf("%11lg ", x[i][j]);
 
 			puts("");
 		}
@@ -111,15 +120,16 @@ int get_inverse(float x[][100], float des[][100], int dim)
 
 		for (int i = 0; i < dim; i++)
 		{
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (modified_row == i) ? 12 : 10);
+
 			for (int j = 0; j < dim; j++)
-				printf("%g ", des[i][j]);
+				printf("%11lg ", des[i][j]);
 
 			puts("");
 		}
 
-		puts("\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
 #endif
-
 	}
 
 	return 1;
@@ -127,7 +137,7 @@ int get_inverse(float x[][100], float des[][100], int dim)
 
 
 //단위행렬 구현
-void make_I(float x[][100], int dim)
+void make_I(double x[][100], int dim)
 {
 	for (int i = 0; i < dim; i++)
 		for (int j = 0; j < dim; j++)
@@ -137,34 +147,14 @@ void make_I(float x[][100], int dim)
 
 int main()
 {
-	/* === 조건 및 주의사항 ===
-	- 입력되는 행렬의 차원은 100보다 작거나 같다.
-	- 각 행렬의 요소는 float 범위를 벗어나지 않는다.
-	- 분수 형태가 아닌 실수 형태로 계산되므로 소수점 이하 자리에서 오차가 발생한다.
-
-
-
-	   === 순서도 ===
-	   1. dim차원 정방행렬 arr에 입력
-	   2. inv 배열을 단위행렬로 변환
-	   3. arr배열을 가우스-요르단 소거법을 통해 각 열을 항등행렬로 변환
-	   4. inv배열에 arr에 적용한 연산 그대로 수행
-	   5. dim번 만큼 3~4과정 반복
-	   6. arr이 대각행렬인지 확인 -> 위의 연산 결과에서 대각행렬이 돼야만 역행렬이 존재
-	   7. arr을 단위행렬로 변환
-	   8. arr에 적용한 연산 inv에 그대로 수행
-	   9. dim번 만큼 7~8과정 반복
-	*/
-
-	
-	system("title Gauss-Jordan elimination method - made by Ji Hun Hong");
+	system("title Gauss-Jordan elimination method - made by https://blog.naver.com/gmail2012");
 	int dim; //입력될 차원 수
-	float arr[100][100], inv[100][100]; //각각 입력받을 행렬, 역행렬을 구하고 저장할 행렬
+	double arr[100][100], inv[100][100]; //각각 입력받을 행렬, 역행렬을 구하고 저장할 행렬
 	scanf("%d", &dim);
 
 	for (int i = 0; i < dim; i++)
 		for (int j = 0; j < dim; j++)
-			scanf("%f", &arr[i][j]);
+			scanf("%lf", &arr[i][j]);
 
 
 	//inv배열을 단위행렬로 만듦 - 가우스 요르단 소거법을 사용하기 위해
@@ -173,24 +163,25 @@ int main()
 	//역행렬 계산
 	get_inverse(arr, inv, dim);
 
-	puts("");
+	puts("\n");
 
 
 	//역행렬이 존재한다면 출력
 	if (get_inverse(arr, inv, dim) == 1)
 	{
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6); //글씨 색 주황
-		
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
+		puts("\n\n\n");
+
 		for (int i = 0; i < dim; i++)
 		{
 			for (int j = 0; j < dim; j++)
-				printf("%10.5f ", inv[i][j]);
+				printf("%11lg ", inv[i][j]);
 
 			puts("");
 		}
 	}
 	else
-		printf("inverse matrix is not exist.");
+		printf("\n\n\ninverse matrix does not exist.");
 
 	puts("");
 
